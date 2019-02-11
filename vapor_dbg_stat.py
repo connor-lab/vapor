2493ca0b4c59bf8f1d332f97289236f3cc54d47e
j <joelsouthgate@googlemail.com>
2019-02-11 13:41:05 +0000

    bugfix

--- vapor_dbg_stat.py
+++ vapor_dbg_stat.py
@@ -102,7 +102,7 @@ def main(args):
         else:
             counts[count] = 1
 
-    for count, val in sorted(counts.items(), key = lambda x:x[1]):
+    for count, n in sorted(counts.items(), key = lambda x:x[1]):
         print("%d,%d" % (count, n))
 
 
